d8f247d13628b0af539dae3a351cfa902d9bdb6c
unknown <jean xavier>
2023-08-25 16:46:03 -0500

    se termina el punto de la tabla de multiplicar

--- src/main/java/com/mycompany/temperaturaconverter/TablaDeMultiplicar.java
+++ src/main/java/com/mycompany/temperaturaconverter/TablaDeMultiplicar.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.temperaturaconverter;
 
+import java.util.Scanner;
+
 /**
- *
- * @author Usuario
+Tabla de Multiplicar.
+Crea un programa que solicite al usuario un número y genere la tabla de multiplicar correspondiente, 
+desde 1 hasta 10.
  */
 public class TablaDeMultiplicar {
-    
+    public static void main(String[] args) {
+        
+        Scanner tabla = new Scanner(System.in);
+        int numero;
+        
+        System.out.println("Ingrese un numero: ");
+        numero = tabla.nextInt();
+        
+        System.out.println("tabla del:" + numero);
+        for(int i = 1; i<=10; i++){
+            System.out.println(numero + "*:" + i + "=" + numero*i);   
+        }     
+    }
 }
